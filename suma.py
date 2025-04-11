9260a23a8451fbce787aee5cc7314ed7d59d411e
YoshuaPariona <loganpariona.1207@gmail.com>
2025-04-11 12:53:21 -0500

    Se implementó la suma

---
+++ suma.py
@@ -0,0 +1,8 @@
+class Suma:
+
+    def __init__(self, operando1, operando2):
+        self.operando1 = operando1
+        self.operando2 = operando2
+
+    def calcularSuma(self):
+        return self.operando1 + self.operando2
